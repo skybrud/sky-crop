393163f74f18f270578fc42eae8f87ed8eff6906
Rudi Ørnhøj <rornhoj@gmail.com>
2017-04-04 07:39:53 +0200

    Bug fixes, scraped style calculation, improved inner workings

--- sky-crop.vue
+++ sky-crop.vue
@@ -1,4 +1,5 @@
 <script>
+	import SkyWindow from 'sky-window';
 	import ImageObject from './SkyCropImage.class';
 
 	export default {
@@ -27,59 +28,84 @@
 				},
 			},
 			mode: String,
-			round: String,
+			round: [String, Number],
 			type: {
 				type: String,
 				default: 'img',
-				required: false,
 			},
+			alt: String,
+			parentId: String,
 		},
 		data() {
 			return {
-				image: new ImageObject(this.src),
+				image: this.image || new ImageObject(this.src),
 				srcString: '',
+				altText: this.alt,
 				styleObject: { },
 			};
 		},
 		methods: {
-			anchorString(image) {
-				if (this.$el.firstChild.localName !== 'img') {
-					this.$set(this.styleObject, 'background-position', `${image.anchor.x} ${image.anchor.y}`);
-				} else {
-					/* Convert anchor % to decimal */
-					const pointX = Number(image.anchor.x.replace('%', '')) / 100;
-					const pointY = Number(image.anchor.y.replace('%', '')) / 100;
-
-					/* Finding difference in size between parent and original image */
-					const differenceX = image.parent.width - image.calculatedInfo.width;
-					const differenceY = image.parent.height - image.calculatedInfo.height;
-
-					/* Setting focal point relative to difference in size */
-					const anchorX = Math.min(0, differenceX * pointX);
-					const anchorY = Math.min(0, differenceY * pointY);
-
-					this.$set(this.styleObject, 'transform', `translate(${anchorX}px, ${anchorY}px)`);
-				}
+			setAnchor(image) {
+				const vm = this;
+				image.calculatedInfo.styles.forEach((style) => {
+					if (style.name === 'transform'
+					|| style.name === 'background-position') {
+						vm.$set(vm.styleObject, style.name, style.value);
+					}
+				});
 			},
-			concatSrc(string) {
-				/* By default always set a string with the output url */
-				this.srcString = string;
+			loadSource(image) {
+				/*
+				 * Delay setting actual source until image url has already been loaded
+				 * to cache by using 'const img' to prefetch
+				 */
+				const img = new Image();
 
-				/* If element is div - use src in a background-image */
-				if (this.type !== 'img') {
-					this.$set(this.styleObject, 'background-image', `url(${this.srcString})`);
+				const load = () => {
+					this.setSource(image);
+
+					/* Cleanup event listeners */
+					img.removeEventListener('load', load);
+					img.removeEventListener('error', load);
+				};
+
+				/* Run load function both when img loads and errors (just for fallback) */
+				img.addEventListener('load', load);
+				img.addEventListener('error', load);
+
+				/* Set img src - now just waiting for events */
+				img.src = image.outputUrl;
+			},
+			setSource(image) {
+				const vm = this;
+
+				/* By default always set a string with the output url */
+				this.srcString = image.outputUrl;
+
+				image.calculatedInfo.styles.forEach((style) => {
+					vm.$set(vm.styleObject, style.name, style.value);
+				});
+			},
+			update() {
+				/* When instance is mounted in DOM, start methods with DOM info dependencies */
+				this.image.setParentInfo(this.$el);
+				this.image.runCropJob(this.type === 'img');
+				if (this.srcString && this.srcString !== this.image.outputUrl) {
+					this.loadSource(this.image);
+				} else {
+					this.setSource(this.image);
 				}
 			},
 		},
 		created() {
-			/**
+			/*
 			 *	When instance is instantiated set image props with attr values
 			 *	- if attr is set the corresponding image property is updated.
 			 */
 			if (this.mode) {
 				this.image.mode = this.mode;
 			}
-			if (this.round) {
+			if (!isNaN(Number(this.round))) {
 				this.image.round = this.round;
 			}
 			if (this.focalpoint) {
@@ -90,11 +116,24 @@
 			}
 		},
 		mounted() {
-			/* When instance is mounted in DOM, start methods with DOM info dependencies */
-			this.image.setParentInfo(this.$el);
-			this.image.runCropJob();
-			this.anchorString(this.image);
-			this.concatSrc(this.image.outputUrl);
+			this.update();
+
+			SkyWindow.resize.subscribe(() => {
+				if (this.$el.clientWidth > this.image.calculatedInfo.width
+				|| this.$el.clientWidth < this.image.calculatedInfo.width - this.image.round
+				|| this.$el.clientHeight > this.image.calculatedInfo.height
+				|| this.$el.clientHeight < this.image.calculatedInfo.height - this.image.round) {
+					/* Rerun entire crop process */
+					this.update();
+				} else {
+					/* Recalculate anchorpoint with the new parent dimensions */
+					this.image.setParentInfo(this.$el);
+				}
+				this.setAnchor(this.image);
+			});
+		},
+		destroy() {
+			SkyWindow.resize.unsubscribe();
 		},
 	};
 </script>
