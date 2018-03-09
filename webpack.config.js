2ab6ec2f91fe3b478d8842560322031950c9d084
Rudi Ørnhøj <rornhoj@gmail.com>
2018-03-09 19:53:44 +0100

    Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const SkyBuildSetup = require('sky-build-setup');
 const PackageJson = require('./package.json');
 const path = require('path');
 
-console.log('MODULE', path.resolve(__dirname));
-
 module.exports = SkyBuildSetup(
 	PackageJson.name,
 	PackageJson.build,
