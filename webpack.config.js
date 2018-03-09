231b6d4b798c6e2ad27f292322cd9feca435fe5a
Rudi Ørnhøj <rornhoj@gmail.com>
2018-03-09 19:46:05 +0100

    Centralized webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,11 @@
-const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const merge = require('webpack-merge');
+const SkyBuildSetup = require('sky-build-setup');
+const PackageJson = require('./package.json');
 const path = require('path');
-const packageJson = require('./package.json');
 
-const name = (() => {
-	const capitalise = (string) => string.charAt(0).toUpperCase() + string.slice(1);
-	const nameArray = packageJson.name.split('-');
+console.log('MODULE', path.resolve(__dirname));
 
-	return `${capitalise(nameArray[0])}${capitalise(nameArray[1])}`;
-})();
-
-const baseConfig = {
-	output: {
-		path: path.resolve(__dirname + '/dist/'),
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				include: __dirname,
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-				options: {
-					preserveWhitespace: false,
-					postcss: [
-						require('autoprefixer')(),
-					],
-					loaders: {
-						css: 'vue-style-loader!css-loader!sass-loader',
-					},
-				},
-			},
-		],
-	},
-	plugins: [
-		new UglifyJsPlugin({
-			sourceMap: false,
-			uglifyOptions: {
-				mangle: true,
-				compress: {
-					warnings: false,
-				},
-			},
-		}),
-	],
-};
-
-const moduleConfig = {
-	target: 'node',
-	entry: path.resolve(__dirname + '/src/' + name + '.vue'),
-	output: {
-		filename: name.toLowerCase() + '.js',
-		libraryTarget: 'umd',
-		library: name,
-		umdNamedDefine: true,
-	},
-}
-
-const pluginConfig = {
-	entry: path.resolve(__dirname + '/src/plugin.js'),
-	output: {
-		filename: name.toLowerCase() + '.min.js',
-		libraryTarget: 'window',
-		library: name,
-	}
-}
-
-const serviceConfig = {
-	entry: path.resolve(__dirname + '/src/' + name + '.js'),
-	output: {
-		filename: name.toLocaleLowerCase() + '.js',
-		libraryTarget: 'umd',
-		library: name,
-		umdNamedDefine: true,
-	},
-}
-
-const config = name === 'SkyWindow'
-	? merge(baseConfig, serviceConfig)
-	: [
-		merge(baseConfig, pluginConfig),
-		merge(baseConfig, moduleConfig),
-	];
-
-module.exports = config;
+module.exports = SkyBuildSetup(
+	PackageJson.name,
+	PackageJson.build,
+	path.resolve(__dirname)
+);
