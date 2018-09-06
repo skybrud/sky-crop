dd1471ec93773f7be603a1adc6d63cfc14e1fc24
Rudi Ørnhøj <rornhoj@gmail.com>
2018-09-06 10:36:37 +0200

    Refactored codebase

--- dist/sky-crop.umd.js
+++ dist/sky-crop.umd.js
@@ -672,9 +672,7 @@
 		};
 	}
 
-	(function(){ if(typeof document !== 'undefined'){ var head=document.head||document.getElementsByTagName('head')[0], style=document.createElement('style'), css=".sky-crop { position: relative; overflow: hidden; } .sky-crop .element { display: block; position: absolute; top: 0; left: 0; margin: 0; will-change: transform; } .sky-crop .element:first-of-type { position: relative; } "; style.type='text/css'; if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style); } })();
-
-	var SkyCrop = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{class:['sky-crop', { 'default': _vm.defaultCrop }]},_vm._l((_vm.imageArray),function(image){return _c('img',{staticClass:"element",style:(image.styling),attrs:{"src":image.src,"alt":_vm.alt},on:{"load":_vm.removeOldElement}})}))},staticRenderFns: [],
+	var script = {
 		name: 'SkyCrop',
 		props: {
 			src: {
@@ -772,6 +770,134 @@
 			resize.off(this.resizeCrop);
 		},
 	};
+
+	/* script */
+	            var __vue_script__ = script;
+	/* template */
+	var __vue_render__ = function() {
+	  var _vm = this;
+	  var _h = _vm.$createElement;
+	  var _c = _vm._self._c || _h;
+	  return _c(
+	    "div",
+	    { class: ["sky-crop", { default: _vm.defaultCrop }] },
+	    _vm._l(_vm.imageArray, function(image) {
+	      return _c("img", {
+	        staticClass: "element",
+	        style: image.styling,
+	        attrs: { src: image.src, alt: _vm.alt },
+	        on: { load: _vm.removeOldElement }
+	      })
+	    })
+	  )
+	};
+	var __vue_staticRenderFns__ = [];
+	__vue_render__._withStripped = true;
+
+	  /* style */
+	  var __vue_inject_styles__ = undefined;
+	  /* scoped */
+	  var __vue_scope_id__ = undefined;
+	  /* module identifier */
+	  var __vue_module_identifier__ = undefined;
+	  /* functional template */
+	  var __vue_is_functional_template__ = false;
+	  /* component normalizer */
+	  function __vue_normalize__(
+	    template, style, script$$1,
+	    scope, functional, moduleIdentifier,
+	    createInjector, createInjectorSSR
+	  ) {
+	    var component = (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {};
+
+	    // For security concerns, we use only base name in production mode.
+	    component.__file = "/Users/rudiornhoj/Sites/roernhoej/sky-crop/src/SkyCrop.vue";
+
+	    if (!component.render) {
+	      component.render = template.render;
+	      component.staticRenderFns = template.staticRenderFns;
+	      component._compiled = true;
+
+	      if (functional) { component.functional = true; }
+	    }
+
+	    component._scopeId = scope;
+
+	    
+
+	    return component
+	  }
+	  /* style inject */
+	  function __vue_create_injector__() {
+	    var head = document.head || document.getElementsByTagName('head')[0];
+	    var styles = __vue_create_injector__.styles || (__vue_create_injector__.styles = {});
+	    var isOldIE =
+	      typeof navigator !== 'undefined' &&
+	      /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
+
+	    return function addStyle(id, css) {
+	      if (document.querySelector('style[data-vue-ssr-id~="' + id + '"]')) { return } // SSR styles are present.
+
+	      var group = isOldIE ? css.media || 'default' : id;
+	      var style = styles[group] || (styles[group] = { ids: [], parts: [], element: undefined });
+
+	      if (!style.ids.includes(id)) {
+	        var code = css.source;
+	        var index = style.ids.length;
+
+	        style.ids.push(id);
+
+	        if (isOldIE) {
+	          style.element = style.element || document.querySelector('style[data-group=' + group + ']');
+	        }
+
+	        if (!style.element) {
+	          var el = style.element = document.createElement('style');
+	          el.type = 'text/css';
+
+	          if (css.media) { el.setAttribute('media', css.media); }
+	          if (isOldIE) {
+	            el.setAttribute('data-group', group);
+	            el.setAttribute('data-next-index', '0');
+	          }
+
+	          head.appendChild(el);
+	        }
+
+	        if (isOldIE) {
+	          index = parseInt(style.element.getAttribute('data-next-index'));
+	          style.element.setAttribute('data-next-index', index + 1);
+	        }
+
+	        if (style.element.styleSheet) {
+	          style.parts.push(code);
+	          style.element.styleSheet.cssText = style.parts
+	            .filter(Boolean)
+	            .join('\n');
+	        } else {
+	          var textNode = document.createTextNode(code);
+	          var nodes = style.element.childNodes;
+	          if (nodes[index]) { style.element.removeChild(nodes[index]); }
+	          if (nodes.length) { style.element.insertBefore(textNode, nodes[index]); }
+	          else { style.element.appendChild(textNode); }
+	        }
+	      }
+	    }
+	  }
+	  /* style inject SSR */
+	  
+
+	  
+	  var SkyCrop = __vue_normalize__(
+	    { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+	    __vue_inject_styles__,
+	    __vue_script__,
+	    __vue_scope_id__,
+	    __vue_is_functional_template__,
+	    __vue_module_identifier__,
+	    __vue_create_injector__,
+	    undefined
+	  )
 
 	var defaults = {
 		registerComponents: true,
