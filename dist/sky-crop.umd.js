1b48b87d1f679c69782668287e6a1d21ef62d57f
Rudi Ørnhøj <rornhoj@gmail.com>
2018-09-06 10:44:58 +0200

    Build changes

--- dist/sky-crop.umd.js
+++ dist/sky-crop.umd.js
@@ -828,62 +828,7 @@
 	    return component
 	  }
 	  /* style inject */
-	  function __vue_create_injector__() {
-	    var head = document.head || document.getElementsByTagName('head')[0];
-	    var styles = __vue_create_injector__.styles || (__vue_create_injector__.styles = {});
-	    var isOldIE =
-	      typeof navigator !== 'undefined' &&
-	      /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
-
-	    return function addStyle(id, css) {
-	      if (document.querySelector('style[data-vue-ssr-id~="' + id + '"]')) { return } // SSR styles are present.
-
-	      var group = isOldIE ? css.media || 'default' : id;
-	      var style = styles[group] || (styles[group] = { ids: [], parts: [], element: undefined });
-
-	      if (!style.ids.includes(id)) {
-	        var code = css.source;
-	        var index = style.ids.length;
-
-	        style.ids.push(id);
-
-	        if (isOldIE) {
-	          style.element = style.element || document.querySelector('style[data-group=' + group + ']');
-	        }
-
-	        if (!style.element) {
-	          var el = style.element = document.createElement('style');
-	          el.type = 'text/css';
-
-	          if (css.media) { el.setAttribute('media', css.media); }
-	          if (isOldIE) {
-	            el.setAttribute('data-group', group);
-	            el.setAttribute('data-next-index', '0');
-	          }
-
-	          head.appendChild(el);
-	        }
-
-	        if (isOldIE) {
-	          index = parseInt(style.element.getAttribute('data-next-index'));
-	          style.element.setAttribute('data-next-index', index + 1);
-	        }
-
-	        if (style.element.styleSheet) {
-	          style.parts.push(code);
-	          style.element.styleSheet.cssText = style.parts
-	            .filter(Boolean)
-	            .join('\n');
-	        } else {
-	          var textNode = document.createTextNode(code);
-	          var nodes = style.element.childNodes;
-	          if (nodes[index]) { style.element.removeChild(nodes[index]); }
-	          if (nodes.length) { style.element.insertBefore(textNode, nodes[index]); }
-	          else { style.element.appendChild(textNode); }
-	        }
-	      }
-	    }
-	  }
+	  
 	  /* style inject SSR */
 	  
 
@@ -895,7 +840,7 @@
 	    __vue_scope_id__,
 	    __vue_is_functional_template__,
 	    __vue_module_identifier__,
-	    __vue_create_injector__,
+	    undefined,
 	    undefined
 	  )
 
